0e927e2c38edc4ee8a673f2f6fc3a5ca5f427625
jooanasilva <jooanasilva042@gmail.com>
2025-02-26 14:48:51 -0300

    Upload dos códigos da primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 21;
+            string nomeCompleto = "Joana Silva";
+
+            var teste = "Joana"; 
+
+            Console.WriteLine(teste.GetType());
+
+            Console.WriteLine("Digite seu nome: ");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+
+            Console.WriteLine($"Meu nome é {nomeUsuario} e minha idade é {idadeUsuario}");
+        }
+    }
+}
